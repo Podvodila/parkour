951b8515d5ef240bb3292389761b5e7bb6b4120a
Podvodila <32092071+Podvodila@users.noreply.github.com>
2018-07-02 18:17:32 +0300

    google-maps-last

--- resources/views/profileEdit.blade.php
+++ resources/views/profileEdit.blade.php
@@ -21,5 +21,5 @@
 @section('scripts')
 	<script src="{{ asset('js/app.js') }}" defer></script>
 	<script async defer src="/js/markerclusterer.js"></script>
-  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAM7zVJIpwF35JDbkg0oS4awX4pBzZoMac"></script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAM7zVJIpwF35JDbkg0oS4awX4pBzZoMac&libraries=places"></script>
 @endsection
