3cb0d0c0b434b7e38cc75db4c8a44a55aa58189b
Ruslan Skoryk <homie.meh@gmail.com>
2020-05-19 19:50:27 +0300

    update composer packages & gitignore

--- app/Comment.php
+++ app/Comment.php
@@ -3,12 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
 
 class Comment extends Model
 {
     protected $fillable = [
         'description', 'spot_id', 'user_id',
     ];
+
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 
     public function spot()
     {
--- app/Video.php
+++ app/Video.php
@@ -3,12 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
 
 class Video extends Model
 {
     protected $fillable = [
         'path', 'user_id', 'trick_id', 'spot_id',
     ];
+
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 
     public function user()
     {
